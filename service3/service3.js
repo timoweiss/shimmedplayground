092e6bd12953841f702178cc26e1d5d6df8f6775
Timo Weiß <info@timo-weiss.com>
2016-07-12 11:32:10 +0200

    format

--- service3/service3.js
+++ service3/service3.js
@@ -7,21 +7,21 @@ require('shimmingtest').start({
 
 const s = require('seneca')();
 
-s.use(function(opts) {
+s.use(function (opts) {
 
     s.add('role:service3', function handlerFuerService2(args, callback) {
         // console.log('USER_SERVICE3: rcv #4', args);
         // setTimeout(function() {
-        console.time('service4antwort')    
-	s.act({role: 'service4'}, function callbackFuerIndex(err, data) {
-                console.log('client_index:', data);
-		console.timeEnd('service4antwort')
-                callback(null, {from:'service3'})
-            })
+        console.time('service4antwort')
+        s.act({role: 'service4'}, function callbackFuerIndex(err, data) {
+            console.log('client_index:', data);
+            console.timeEnd('service4antwort')
+            callback(null, {from: 'service3'})
+        })
         // },1|| Math.floor(Math.random() * 100))
 
     });
     return {name: 'plugin-name-service3'}
 });
 
-s.use('mesh', {auto:true, pins: ['role:service3']});
+s.use('mesh', {auto: true, pins: ['role:service3']});
